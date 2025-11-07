6f1474020e5769f22a33a1cfa817b611100608f2
Yvani <yvanilemes@gmail.com>
2025-11-07 19:32:27 -0300

    Add arquivos com modificações ao projeto

--- MauiAppHotel/Views/ContratacaoHospedagem.xaml.cs
+++ MauiAppHotel/Views/ContratacaoHospedagem.xaml.cs
@@ -14,4 +14,17 @@ public partial class ContratacaoHospedagem : ContentPage
 
 	}
 
-}
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+		try
+		{
+			Navigation.PushAsync(new HospedagemContratada());
+
+		} catch (Exception ex)
+			{
+			DisplayAlert("Ops", ex.Message, "Ok");
+		}
+
+		}
+    }
+
