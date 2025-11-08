8f044c8db8284a2483cdf94f0b63b640a7e5008a
Yvani <yvanilemes@gmail.com>
2025-11-07 21:38:52 -0300

    Add modificacoes/Alterações no projeto

--- MauiAppHotel/App.xaml.cs
+++ MauiAppHotel/App.xaml.cs
@@ -1,8 +1,39 @@
 ﻿
+using MauiAppHotel.Models;
+
 namespace MauiAppHotel
 {
     public partial class App : Application
     {
+        public List<Quarto> lista_quartos = new List<Quarto>
+        {
+        new Quarto()
+        {
+            Descrição = "Suíte Super Luxo",
+            ValorDiariaAdulto = 110.0,
+            ValorDiariaCriança = 55.0,
+            
+        },
+        new Quarto()
+        {
+            Descrição = "Suíte Luxo",
+            ValorDiariaAdulto = 80.0,
+            ValorDiariaCriança = 40.0
+        },
+        new Quarto()
+        {
+            Descrição = "Suíte Single",
+            ValorDiariaAdulto = 50.0,
+            ValorDiariaCriança = 25
+        },
+        new Quarto()
+        {
+            Descrição = "Suíte Crise",
+            ValorDiariaAdulto = 25.0,
+            ValorDiariaCriança = 12.5
+        }
+};
+
         public App()
         {
             InitializeComponent();
